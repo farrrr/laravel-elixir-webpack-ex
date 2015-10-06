936a7711b81e8ff99379fd7df92eaa45212cb3df
Far Tseng <farrrr@gmail.com>
2015-10-06 20:32:41 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,60 @@
+# Laravel-Elixir-Webpack-Ex
+
+Laravel-Elixir [webpack](https://github.com/webpack/webpack) extension.
+
+## Install
+```
+nam install laravel-elixir-webpack-ex —save
+```
+
+## Usage
+
+### Simple Example
+
+```js
+var elixir = require(‘laravel-elixir’);
+var gulp = require(‘gulp’);
+
+require(‘laravel-elixir-webpack-ex’);
+
+elixir(function(mix) {
+  mix.webpack(‘entry.js’, {
+    module: {
+      loaders: [
+        { test: /\.css$/, loader: ‘style!css’ },
+      ],
+    },
+  }, ‘public/js’, ‘resources/assets/js’);
+});
+```
+
+`public/js` is output file path, `resources/assets/js` is assets baseDir, both all optional.
+
+
+You can pass your `webpack.config.js`:
+
+```js
+mix.webpack(‘entry.js’, require(‘./webpack.config.js’));
+```
+
+### Multiple Entry Points
+
+You can pass entries array:
+
+```js
+mix.webpack(['entry1.js', 'entry2.js', 'entry3.js'], { ...webpackOptions });
+```
+
+or Pass an `entry` option in it:
+
+```js
+mix.webpack({
+  App: 'entry1.js',
+  Backend: 'entry2.js',
+  Dashboard: 'entry3.js,
+}, {...webpackOptions });
+```
+
+## License
+Copyright (c) 2015 Far Tseng
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var path = require('path');
+var Elixir = require('laravel-elixir');
+var webpack = require('webpack-stream');
+var named = require('vinyl-named');
+var _ = require('lodash');
+
+var Task = Elixir.Task;
+var Notification = Elixir.Notification;
+var config = Elixir.config;
+var $ = Elixir.Plugins;
+
+Elixir.extend('webpack', function(src, options, output, baseDir) {
+  var paths = prepGulpPaths(src, baseDir, output);
+
+  if (_.isPlainObject(src)) {
+    var entry = _.mapValues(src, function(script) {
+      return './' + path.join(paths.src.baseDir, script);
+    });
+
+    options = _.assign({}, options, {
+      entry: entry,
+      output: { filename: '[name].js' },
+    });
+  }
+
+  new Task('webpack', function() {
+    var saveFiles;
+
+    if (_.isArray(src)) {
+      saveFiles = _.map(paths.src.path, function(file) {
+        return path.join(paths.output.baseDir, path.basename(file, path.extname(file)) + '.js');
+      });
+    } else if (_.isPlainObject(src)) {
+      saveFiles = _.map(_.keys(src), function(file) {
+        return path.join(paths.output.baseDir, file + '.js');
+      });
+    } else {
+      saveFiles = paths.output;
+    }
+
+    this.log(paths.src, saveFiles);
+
+    return gulp.src(paths.src.path)
+      .pipe(named())
+      .pipe(webpack(options))
+      .on('error', function(e) {
+        (new Notification).error(err, 'Webpack Compilation Failed!');
+
+        this.emit('end');
+      })
+      .pipe($.if(config.production,  $.uglify()))
+      .pipe(gulp.dest(paths.output.baseDir))
+      .pipe(new Elixir.Notification('Webpack Compiled!'));
+  })
+  .watch(paths.src.baseDir + '/**/*');
+});
+
+/**
+ * Prep the Gulp src and output paths.
+ *
+ * @param  {string|array} src
+ * @param  {string}       baseDir
+ * @param  {string|null}  output
+ */
+var prepGulpPaths = function(src, baseDir, output) {
+  baseDir = baseDir || config.get('assets.js.folder');
+
+  if (_.isObject(src)) {
+    src = _.values(src);
+  }
+
+  return new Elixir.GulpPaths()
+    .src(src, baseDir)
+    .output(output || config.get('public.js.outputFolder'), 'app.js');
+};
